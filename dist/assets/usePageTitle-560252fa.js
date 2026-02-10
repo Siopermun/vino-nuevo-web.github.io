9fe5fd33df369a21fef0bc748a2b9fddeaf3271a
Siopermun <165212535+Siopermun@users.noreply.github.com>
2026-02-10 19:01:22 +0000

    Web lista para publicar-

---
+++ dist/assets/usePageTitle-560252fa.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-33c4326b.js";const c=t=>{o.useEffect(()=>{const e=document.title;return document.title=t,()=>{document.title=e}},[t])};export{c as u};
--- dist/assets/usePageTitle-c7f681f8.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-8b0506b8.js";const c=t=>{o.useEffect(()=>{const e=document.title;return document.title=t,()=>{document.title=e}},[t])};export{c as u};
