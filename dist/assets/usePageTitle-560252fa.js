f9862ce68230e6113b246cc443c47546d1a20cbd
Siopermun <165212535+Siopermun@users.noreply.github.com>
2026-02-10 22:19:28 +0000

    WEB configurada correctamente

--- dist/assets/usePageTitle-560252fa.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-33c4326b.js";const c=t=>{o.useEffect(()=>{const e=document.title;return document.title=t,()=>{document.title=e}},[t])};export{c as u};
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], base: '/Vino-nuevo-web.github.io/', //´
+  plugins: [react()], base: '/', 
 })
